e5492878eb12423a99bdfda61298be3979698549
Gustavo Montamat <gmontamat@gmail.com>
2017-07-03 23:30:26 -0300

    Add code to detect complementary columns

---
+++ scripts/complementary_columns.R
@@ -0,0 +1,67 @@
+# Set current working directory
+setwd("~/Documents/kaggle/mercedes-benz-greener-manufacturing")
+
+# Source required scripts
+source("./scripts/data_load.R")
+
+# Remove columns with only one level
+filter <- sapply(train[-which(names(train) %in% not_factors)], nlevels) > 1
+train <- train[, c(TRUE, TRUE, filter)]
+test <- test[, c(TRUE, filter)]
+
+# Remove features X0 through X8
+train <- train[, -which(names(train) %in% c("X0","X1", "X2", "X3", "X4", "X5", "X6", "X8"))]
+test <- test[, -which(names(test) %in% c("X0","X1", "X2", "X3", "X4", "X5", "X6", "X8"))]
+
+# Remove outlier
+train <- train[train$y<250,]
+
+# Find columns with the same content
+duplicate_columns <- c()
+for (name1 in names(train)) {
+  for (name2 in names(train)) {
+    if (name1<name2 && identical(train[[name1]], train[[name2]])) {
+      duplicate_columns <- c(duplicate_columns, name2)
+    }
+  }
+}
+duplicate_columns <- unique(duplicate_columns)
+train <- train[, -which(names(train) %in% duplicate_columns)]
+test <- test[, -which(names(test) %in% duplicate_columns)]
+
+# Find groups of three columns which are complementary and group into a factor
+for (name1 in names(train)) {
+  for (name2 in names(train)) {
+    for (name3 in names(train)) {
+      if (name1<name2 && name2<name3 &&
+          sum(as.numeric(as.character(train[[name1]]))) +
+          sum(as.numeric(as.character(train[[name2]]))) +
+          sum(as.numeric(as.character(train[[name3]]))) == rows) {
+        if (all(xor(xor(as.numeric(as.character(train[[name1]])),
+                        as.numeric(as.character(train[[name2]]))),
+                    as.numeric(as.character(train[[name3]]))))) {
+          print(c(name1, name2, name3))
+        }
+      }
+    }
+  }
+}
+
+# Find and drop pairs of columns that complement each other
+rows <- nrow(train)
+complementary_cols <- c()
+for (name1 in names(train)) {
+  for (name2 in names(train)) {
+    if (name1<name2 &&
+        sum(as.numeric(as.character(train[[name1]]))) +
+        sum(as.numeric(as.character(train[[name2]]))) == rows) {
+      if (all(xor(as.numeric(as.character(train[[name1]])),
+                  as.numeric(as.character(train[[name2]]))))) {
+        print(paste0(name1, " is a complement of ", name2))
+        complementary_cols <- c(complementary_cols, name2)
+      }
+    }
+  }
+}
+train <- train[, -which(names(train) %in% complementary_cols)]
+test <- test[, -which(names(test) %in% complementary_cols)]

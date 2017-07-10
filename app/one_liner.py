ec9f1a6bfb95c3549f09cf076710475c99a06bc4
Gustavo Montamat <gmontamat@gmail.com>
2017-07-09 22:40:06 -0300

    Change output file name

--- app/one_liner.py
+++ app/one_liner.py
@@ -10,4 +10,4 @@ import pandas as pd
 if __name__ == '__main__':
     test = pd.read_csv('../data/test.csv')
     test = test.join(pd.read_csv('../data/train.csv').groupby(['X0'])[['y']].mean(), on=['X0']).fillna(100.669)
-    test.to_csv('../data/submission.csv', columns=['ID', 'y'], index=False)
+    test.to_csv('../data/submission_one-liner.csv', columns=['ID', 'y'], index=False)
